cc1f4d4d90e100b5803ecefdce424f70c75e477a
Fatma072 <ftmx.na@gmail.com>
2025-07-19 14:20:14 +0700

    Create 1_Spektrofotometer.py

---
+++ pages /1_Spektrofotometer.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("🔬 Simulator Spektrofotometer")
+st.write("""
+Simulasi spektrofotometer untuk memahami hukum Beer-Lambert dan grafik absorbansi.
+""")
+import streamlit as st
+
+st.title("🔬 Simulator Spektrofotometer")
+st.write("""
+Simulasi spektrofotometer untuk memahami hukum Beer-Lambert dan grafik absorbansi.
+""")
