ca0a61afde6d7bb446c497e85421412e433a14fa
Fatma072 <ftmx.na@gmail.com>
2025-07-19 14:27:36 +0700

    Delete pages  directory

--- pages /4_Penanganan_Bahan_Kimia.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("🧴 Penanganan Bahan Kimia")
-st.write("Panduan menyimpan, menggunakan, dan membuang bahan kimia secara aman.")
--- pages /5_Keselamatan_Kerja_K3.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("🛡️ Keselamatan dan Kesehatan Kerja (K3)")
-st.write("Informasi tentang prosedur keselamatan laboratorium dan penggunaan APD.")
